cf0f69ff0e3d2b480b59a404b4489fc3a5db957a
十号 <251895921@qq.com>
2022-08-15 20:19:48 +0800

    解决冲突

--- jiekou.ts
+++ jiekou.ts
@@ -24,17 +24,6 @@ initLab(lab)
 // 额外属性检查，当将它们赋值给变量或作为参数传递的时候。 如果一个对象字面量存在任何“目标类型”不包含的属性时，你会得到一个错误。
 //  解决方法： 1 [m1]添加一个字符串索引签名  2 [m2] 赋值给一个变量 initLab(lab)
 initLab ({label: " I am string", age: 13, sex: "1"})
-
-
-
-
-
-
-
-
-
-
-
 // a
 
 // b
@@ -58,25 +47,9 @@ initLab ({label: " I am string", age: 13, sex: "1"})
 // 13 git cherry-pick b3d0ea
 
 // 14
-=======
 // 接口
 interface LabelVal {
   label: string
   name?: string // 可选参数
   readonly age ?: number
 }
-function initLab( label: LabelVal ) {
-  console.log(label);
-  let obj = {name: "I am obj"}
-  if (label.name) {
-    obj.name = label.name
-  }
-  console.log(obj);
-  // label.age = 13 只读属性
-  return obj
-  
-}
-
-let lab = {label: " I am string", age: 13, sex: "1"} // [m2]
-
-initLab(lab)
